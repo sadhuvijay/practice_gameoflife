0792bdcb934f03f2263a6b0ebbf0349a53e8c5cf
sadhuvijay <svjsvj4@gmail.com>
2018-03-05 16:11:01 +0530

    Update automateAMI1.sh

--- automateAMI1.sh
+++ automateAMI1.sh
@@ -26,7 +26,7 @@ for instance_info in `cat $instanceID`
 do
 inst_ID=`echo $instance_info | awk -F":" '{print $1}'`
 inst_name=`echo $instance_info | awk -F":" '{print $2}'`
-echo "$inst_name-`date +%d-%m-%y --date '1 days ago'`" >> /tmp/amidel1.txt
+echo "$inst_name-`date +%d-%m-%y --date '0 days ago'`" >> /tmp/amidel1.txt
 done
 if [ -f /tmp/amidel1.txt ]
 then
